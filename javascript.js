8c220167fd2fe5eca882fbb43653b367a3e7d80c
ImExtraPolite <jaleel1666@gmail.com>
2023-02-04 16:48:39 -0500

    make changes

---
+++ javascript.js
@@ -0,0 +1,58 @@
+function getComputerChoice() {
+  let randomNum = Math.floor(Math.random() * 3) + 1;
+
+  if (randomNum == 1) {
+    return "rock";
+  } else if (randomNum == 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+  
+  if(playerSelection == "rock" && computerSelection == "rock") {
+    return "tie!";
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You lose! Paper beats rock."
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You win! Rock beats scissors";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You win! Paper beats rock.";
+  } else if (playerSelection == "paper" && computerSelection == "paper") {
+    return "tie!";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You lose! scissors beat paper.";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You lose! Rock beat scissors";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You win! scissors beats paper";
+  } else {
+    return "tie!";
+  }
+}
+
+const rock = document.querySelector(".rockButton");
+const paper = document.querySelector(".paperButton");
+const scissors = document.querySelector(".scissorsButton");
+const win = document.querySelector(".winPoints");
+const lose = document.querySelector(".losePoints");
+const tie = document.querySelector(".tiePoints");
+let w = 0;
+let l = 0;
+let t = 0;
+
+
+rock.addEventListener("click", () => {
+  console.log(playRound(rock.textContent, getComputerChoice()));
+});
+
+paper.addEventListener("click", () => {
+  console.log(playRound(paper.textContent, getComputerChoice()));
+});
+
+scissors.addEventListener("click", () => {
+  console.log(playRound(scissors.textContent, getComputerChoice()));
+});
