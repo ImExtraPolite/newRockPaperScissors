4029017ecc7495c5fb1c00e65edb3f0976fde7f5
ImExtraPolite <jaleel1666@gmail.com>
2023-02-03 21:27:32 -0500

    make changes to html and javascript

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissor = document.querySelector(".scissors");
-const score = document.querySelector(".changeUserScore");
+const w = document.querySelector(".winScore");
+const l = document.querySelector(".loseScore")
+const t = document.querySelector(".tieScore")
 let computerChoice;
 let winScore = 0;
-
+let loseScore = 0;
+let tieScore = 0;
+let totalScore = winScore + loseScore + tieScore;
 
 // 1 = Rock
 // 2 = Paper
@@ -22,12 +26,85 @@ function computer() {
   }
 }
 
-
 rock.addEventListener("click", () => {
   let newChoice = computer();
 
   if(newChoice == 1) {
-    score.textContent = winScore = winScore + 1;
+    t.textContent = tieScore = tieScore + 1;
+    console.log(newChoice + "rock");
+
   }
-  console.log(newChoice);
-})  
+}) 
+
+paper.addEventListener("click", () => {
+  let newChoice = computer();
+
+  if(newChoice == 1) {
+    w.textContent = winScore = winScore + 1;
+    console.log(newChoice + "rock");
+  }
+})
+
+scissor.addEventListener("click", () => {
+  let newChoice = computer();
+
+  if(newChoice == 1) {
+    l.textContent = loseScore = loseScore + 1;
+    console.log(newChoice + "rock");
+  }
+})
+
+rock.addEventListener("click", () => {
+  let newChoice = computer();
+
+  if(newChoice == 2) {
+    l.textContent = loseScore = loseScore + 1;
+    console.log(newChoice + "paper");
+  }
+}) 
+
+paper.addEventListener("click", () => {
+  let newChoice = computer();
+
+  if(newChoice == 2) {
+    t.textContent = tieScore = tieScore + 1;
+    console.log(newChoice + "paper");
+  }
+}) 
+
+scissor.addEventListener("click", () => {
+  let newChoice = computer();
+
+  if(newChoice == 2) {
+    w.textContent = winScore = winScore + 1;
+    console.log(newChoice + "paper");
+  }
+}) 
+
+rock.addEventListener("click", () => {
+  let newChoice = computer();
+
+  if(newChoice == 3) {
+    w.textContent = winScore = winScore + 1;
+    console.log(newChoice + "scissor");
+  }
+}) 
+
+paper.addEventListener("click", () => {
+  let newChoice = computer();
+
+  if(newChoice == 3) {
+    l.textContent = loseScore = loseScore + 1;
+    console.log(newChoice + "scissor");
+  }
+}) 
+
+scissor.addEventListener("click", () => {
+  let newChoice = computer();
+
+  if(newChoice == 3) {
+    t.textContent = tieScore = tieScore + 1;
+    console.log(newChoice + "scissor");
+  }
+}) 
+
